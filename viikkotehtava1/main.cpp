3f2d6df4f2eb087f0334f121fb81dcc184c7f222
tuomok1010 <tuomok1010@gmail.com>
2026-01-08 12:59:25 +0200

    viikkotehtava1 done

---
+++ viikkotehtava1/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int game(int max_num)
+{
+    // Get a different random number each time the program runs
+    srand(time(0));
+
+    // Generate a random number between 0 and 100
+    int random_num = 1 + (rand() % (max_num + 1));
+    int num_guesses = 0;
+    int guess = 0;
+
+    // Ask user input
+    while(guess != random_num)
+    {
+        ++num_guesses;
+
+        std::cout << "enter your guess (" << 1 << "-" << max_num << ")> ";
+        std::cin.clear();
+        std::cin >> guess;
+
+        if(guess < random_num)
+            std::cout << "your guess was too low!" << std::endl;
+        else if(guess > random_num)
+            std::cout << "your guess was too high!" << std::endl;
+        else
+            std::cout << "you guessed correct!" << std::endl;
+    }
+
+    return num_guesses;
+}
+
+int main()
+{
+    int result = game(5);
+    std::cout << "you made " << result << " number of guesses." << std::endl;
+
+    return 0;
+}
